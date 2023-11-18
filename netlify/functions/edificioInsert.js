4ef2ac408bc2306d0cfb5b35cd1f3d0824257157
jcmarinc1980 <juan.marin.cascante@est.una.ac.cr>
2023-11-18 17:43:50 -0600

    insert

--- netlify/functions/edificioInsert.js
+++ netlify/functions/edificioInsert.js
@@ -1,4 +1,4 @@
-"use strict"
+/*"use strict"
 
 const headers = require('./headersCORS');
 
@@ -20,5 +20,35 @@ exports.handler = async (event, context) => {
   } catch (error) {
     console.log(error);
     return { statusCode: 422, headers, body: JSON.stringify(error) };
+  }
+};
+*/
+
+"use strict"
+const redis = require('./redisDB');
+const headers = require('./headersCORS');
+function toJson(item, index, arr) {
+  arr[index] = JSON.parse(item);
+}
+exports.handler = async (event, context) => {
+  if (event.httpMethod == "OPTIONS") {
+    return { statusCode: 200, headers, body: "OK" };
+  }
+  try {
+    
+    redis.on("connect", function() {
+      console.log("You are now connected");
+    });
+   
+   const data = JSON.parse(event.body);
+   const result = await redis.set('edificios_'+data.id,event.body);
+   console.log({result});
+   if(result)
+     await redis.incr('edificios_N');
+
+   return { statusCode: 200, headers, body: 'OK'};
+  } catch (error) {
+    console.log(error);
+    return { statusCode: 400, headers, body: JSON.stringify(error) };
   }
 };
