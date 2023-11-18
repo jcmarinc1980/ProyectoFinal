9a61a81320fbc1b90a6f41805759c560d7fb9ad9
jcmarinc1980 <juan.marin.cascante@est.una.ac.cr>
2023-11-18 15:46:32 -0600

    Update edificioFind.js

--- netlify/functions/edificioFind.js
+++ netlify/functions/edificioFind.js
@@ -1,4 +1,4 @@
-"use strict"
+/*"use strict"
 
 const clientPromise = require('./mongoDB');
 const headers = require('./headersCORS');
@@ -17,6 +17,34 @@ exports.handler = async (event, context) => {
 	  await client.db("proyecto").collection("edificios").find({_id:id}).toArray();
 
     return { statusCode: 200, headers, body: JSON.stringify(authors)};
+  } catch (error) {
+    console.log(error);
+    return { statusCode: 400, headers, body: JSON.stringify(error) };
+  }
+};*/
+
+"use strict"
+const redis = require('./redisDB');
+const headers = require('./headersCORS');
+function toJson(item, index, arr) {
+  arr[index] = JSON.parse(item);
+}
+exports.handler = async (event, context) => {
+  if (event.httpMethod == "OPTIONS") {
+    return { statusCode: 200, headers, body: "OK" };
+  }
+  try {
+    
+    const id = event.path.split("/").reverse()[0];
+    
+    redis.on("connect", function() {
+      console.log("You are now connected");
+    });
+   const edificio = await redis.get('edificios_'+id);
+   let edificios = [];
+   edificios.push(edificio);
+   edificios.forEach(toJson);
+    return { statusCode: 200, headers, body: JSON.stringify(edificios)};
   } catch (error) {
     console.log(error);
     return { statusCode: 400, headers, body: JSON.stringify(error) };
