0b73a764a26204c4bd87f4673d5f5ce3f4fa59e8
jcmarinc1980 <juan.marin.cascante@est.una.ac.cr>
2023-11-18 16:18:56 -0600

    update

--- netlify/functions/arquitectoUpdate.js
+++ netlify/functions/arquitectoUpdate.js
@@ -1,4 +1,4 @@
-"use strict"
+/*"use strict"
 
 // const clientPromise = require('./mongoDB');
 const headers = require('./headersCORS');
@@ -22,5 +22,26 @@ exports.handler = async (event, context) => {
   } catch (error) {
     console.log(error);
     return { statusCode: 422, headers, body: JSON.stringify(error) };
+  }
+};*/
+
+"use strict"
+const redis = require('./redisDB');
+const headers = require('./headersCORS');
+exports.handler = async (event, context) => {
+  if (event.httpMethod == "OPTIONS") {
+    return { statusCode: 200, headers, body: "OK" };
+  }
+  try {
+    const id = event.path.split("/").reverse()[0];
+    redis.on("connect", function() {
+      console.log("You are now connected");
+    });
+   const result = await redis.set('arquitectos_'+id,event.body);
+   console.log({result})//si es exitoso muestra 'OK'
+   return { statusCode: 200, headers, body: 'OK'};
+  } catch (error) {
+    console.log(error);
+    return { statusCode: 400, headers, body: JSON.stringify(error) };
   }
 };
