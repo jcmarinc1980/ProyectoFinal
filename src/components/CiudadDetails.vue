c25dfa5c2e3a01e33ea13ab755e5ae48bcd4082a
jcmarinc1980 <juan.marin.cascante@est.una.ac.cr>
2023-11-18 20:35:22 -0600

    Update CiudadDetails.vue

--- src/components/CiudadDetails.vue
+++ src/components/CiudadDetails.vue
@@ -80,7 +80,7 @@ export default {
         })
     },
     allEdificios: function(id) {
-      fetch(this.url + '/.netlify/functions/FindAllCiudadEdificio/'+ id,
+      fetch(this.url + '.netlify/functions/FindAllCiudadEdificio/'+ id,
         { headers: { 'Accept': 'application/json' } })
         .then((response) => response.json())
         .then((items) => {
