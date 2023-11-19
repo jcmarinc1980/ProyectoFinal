fd40d07775c94231cf34208b703caacd1bb2d9bd
jcmarinc1980 <juan.marin.cascante@est.una.ac.cr>
2023-11-18 18:13:35 -0600

    rr

--- netlify/functions/FindAllArquitectoEdificio.js
+++ netlify/functions/FindAllArquitectoEdificio.js
@@ -1,4 +1,4 @@
-"use strict"
+/*"use strict"
 
 const clientPromise = require('./mongoDB');
 const headers = require('./headersCORS');
@@ -17,6 +17,37 @@ exports.handler = async (event, context) => {
     const respuesta = await client.db("proyecto").collection("edificios").find({arquitecto_id:id}).toArray();
 
     return { statusCode: 200, headers, body: JSON.stringify(respuesta)};
+  } catch (error) {
+    console.log(error);
+    return { statusCode: 400, headers, body: JSON.stringify(error) };
+  }
+};*/
+
+"use strict";
+
+const redis = require('./redisDB');
+const headers = require('./headersCORS');
+
+function toJson(item, index, arr) {
+  arr[index] = JSON.parse(item);
+}
+
+exports.handler = async (event, context) => {
+  if (event.httpMethod == "OPTIONS") {
+    return { statusCode: 200, headers, body: "OK" };
+  }
+
+  try {
+    redis.on("connect", function() {
+      console.log("You are now connected to Redis");
+    });
+
+    const id = parseInt(event.path.split("/").reverse()[0]);
+    const keys = (await redis.keys(`arquitectos_${id}_*`)).filter(id => id !== `arquitectos_${id}_N`);
+    const arquitectos = await redis.mget(keys);
+
+    arquitectos.forEach(toJson);
+    return { statusCode: 200, headers, body: JSON.stringify(arquitectos)};
   } catch (error) {
     console.log(error);
     return { statusCode: 400, headers, body: JSON.stringify(error) };
--- netlify/functions/FindAllCiudadEdificio.js
+++ netlify/functions/FindAllCiudadEdificio.js
@@ -1,4 +1,4 @@
-"use strict"
+/*"use strict"
 
 const clientPromise = require('./mongoDB');
 const headers = require('./headersCORS');
@@ -17,6 +17,37 @@ exports.handler = async (event, context) => {
     const respuesta = await client.db("proyecto").collection("edificios").find({ciudad_id:id}).toArray();
 
     return { statusCode: 200, headers, body: JSON.stringify(respuesta)};
+  } catch (error) {
+    console.log(error);
+    return { statusCode: 400, headers, body: JSON.stringify(error) };
+  }
+};*/
+
+"use strict";
+
+const redis = require('./redisDB');
+const headers = require('./headersCORS');
+
+function toJson(item, index, arr) {
+  arr[index] = JSON.parse(item);
+}
+
+exports.handler = async (event, context) => {
+  if (event.httpMethod == "OPTIONS") {
+    return { statusCode: 200, headers, body: "OK" };
+  }
+
+  try {
+    redis.on("connect", function() {
+      console.log("You are now connected to Redis");
+    });
+
+    const id = parseInt(event.path.split("/").reverse()[0]);
+    const keys = (await redis.keys(`edificios_${id}_*`)).filter(id => id !== `edificios_${id}_N`);
+    const edificios = await redis.mget(keys);
+
+    edificios.forEach(toJson);
+    return { statusCode: 200, headers, body: JSON.stringify(edificios)};
   } catch (error) {
     console.log(error);
     return { statusCode: 400, headers, body: JSON.stringify(error) };
